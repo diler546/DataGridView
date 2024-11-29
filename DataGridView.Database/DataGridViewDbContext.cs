e1f0d12e8091a4b363862eb633c1c9c144f9aa6b
diler546 <makar912@mail.ru>
2024-11-29 05:10:16 +0300

    add database

---
+++ DataGridView.Database/DataGridViewDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using DataGridView.Contracts.Models;
+
+namespace DataGridView.Database
+{
+    public class DataGridViewDbContext : DbContext
+    {
+        /// <summary>
+        /// Конструктор контекста базы данных
+        /// </summary>
+        public DataGridViewDbContext() : base("DataGridConnectionString")
+        {
+        }
+
+        /// <summary>
+        /// Таблица <see cref="Product"/> в базе данных
+        /// </summary>
+        public DbSet<Product> Product { get; set; }
+    }
+}
